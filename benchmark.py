0b1a318b4141b55ba7a85e9fe1b040f8f79987c6
Phus Lu <phus.lu@gmail.com>
2011-10-08 18:42:42 +0800

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,9 @@
 
 import time
 
+CONNECTIONS = 100
+INCR_NUMBER = 1000
+
 def test_redispy():
     import threading, redis
     redis_client = redis.Redis()
@@ -11,9 +14,9 @@ def test_redispy():
     print 'test_redispy begin', time_begin
     def worker():
         redis_client = redis.Redis()
-        for i in xrange(10000):
+        for i in xrange(INCR_NUMBER):
             redis_client.incr('x')
-    jobs = [threading.Thread(target=worker) for i in xrange(50)]
+    jobs = [threading.Thread(target=worker) for i in xrange(CONNECTIONS)]
     for job in jobs:
         job.start()
     for job in jobs:
@@ -31,9 +34,9 @@ def test_geventredis():
     print 'test_geventredis begin', time_begin
     def worker():
         redis_client = geventredis.connect()
-        for i in xrange(10000):
+        for i in xrange(INCR_NUMBER):
             redis_client.incr('x')
-    jobs = [gevent.spawn(worker) for i in xrange(50)]
+    jobs = [gevent.spawn(worker) for i in xrange(CONNECTIONS)]
     gevent.joinall(jobs)
     time_end = time.time()
     print 'test_geventredis end', time_end
