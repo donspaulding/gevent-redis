fbb8c58cda6c3518d901a49223d25a1f7b495660
Phus Lu <phus.lu@gmail.com>
2011-10-16 22:03:06 +0800

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -7,6 +7,8 @@ INCR_NUMBER = 1000
 
 def test_redispy():
     import threading, redis
+    import gevent.monkey
+    gevent.monkey.patch_all()
     redis_client = redis.Redis()
     redis_client.delete('x')
     del redis_client
@@ -24,28 +26,6 @@ def test_redispy():
     time_end = time.time()
     print 'test_redispy end', time_end
     print 'test_redispy total', time_end - time_begin
-
-def test_geventredis_without_gevent():
-    import gevent, gevent.socket, socket
-    gevent.socket = socket
-    import threading, geventredis
-    redis_client = geventredis.connect()
-    redis_client.delete('x')
-    del redis_client
-    time_begin = time.time()
-    print 'test_geventredis_without_gevent begin', time_begin
-    def worker():
-        redis_client = geventredis.connect()
-        for i in xrange(INCR_NUMBER):
-            redis_client.incr('x')
-    jobs = [threading.Thread(target=worker) for i in xrange(CONNECTIONS)]
-    for job in jobs:
-        job.start()
-    for job in jobs:
-        job.join()
-    time_end = time.time()
-    print 'test_geventredis_without_gevent end', time_end
-    print 'test_geventredis_without_gevent total', time_end - time_begin
 
 def test_geventredis():
     import gevent, geventredis
@@ -66,11 +46,9 @@ def test_geventredis():
 
 def test():
     print '-----------------------------'
-    test_redispy()
-    print '-----------------------------'
-    test_geventredis_without_gevent()
-    print '-----------------------------'
     test_geventredis()
+    print '-----------------------------'
+    test_redispy()
     print '-----------------------------'
 
 if __name__ == '__main__':
